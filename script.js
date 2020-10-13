bd56be872e178ed072c07049bea91638c4199394
waleedomega <waleedomega@hotmail.com>
2020-10-13 11:35:31 +0200

    complete

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const displayInput = document.getElementsByName('display')[0];
 const buttons = document.querySelectorAll('input[type="button"]')
-console.log(buttons);
+
+
+
+const displayOrCalc = (e) => {
+    if (e.target.value === '=') {
+        if (displayInput.value) {
+            displayInput.value = eval(displayInput.value);
+        }
+    } else {
+        if (e.target.value == 'C') {
+            displayInput.value = '';
+        } else if (e.target.value == '÷') {
+            displayInput.value += '/';
+        } else if (e.target.value == 'x') {
+            displayInput.value += '*';
+        } else {
+            displayInput.value += e.target.value;
+        }
+    }
+}
+
+for (let i = 0; i < buttons.length;i++){
+    buttons[i].addEventListener('click',displayOrCalc)
+}
